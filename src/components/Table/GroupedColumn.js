415417755d358b86cf85daf911dac483be84f8dc
Hrabowskie <rjhrabowskie@gmail.com>
2018-11-12 15:54:33 -0500

    Clean state

--- src/components/Table/GroupedColumn.js
+++ src/components/Table/GroupedColumn.js
@@ -1,24 +1,24 @@
-import PropTypes from 'prop-types'
-import React from 'react'
+import PropTypes from "prop-types";
+import React from "react";
 
-import { sparkClassName } from '../../util'
+import { sparkClassName } from "../../util";
 
 class GroupedColumn extends React.Component {
   static defaultProps = {
     children: null
-  }
+  };
 
   static propTypes = {
     children: PropTypes.node
-  }
+  };
 
   render = () => {
-    const {children} = this.props
+    const { children } = this.props;
 
-    const className = sparkClassName('base', 'Table', null, 'grouped-column')
+    const className = sparkClassName("base", "Table", null, "grouped-column");
 
-    return (<th className={className}>{children}</th>)
-  }
+    return <th className={className}>{children}</th>;
+  };
 }
 
-export default GroupedColumn
+export default GroupedColumn;
