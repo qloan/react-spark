14fecbd1d5c65f77841cbf78dccd68bded7b9f6c
Istvan (Steve) Ghidro <steveghidro@quickenloans.com>
2018-11-14 09:51:53 -0500

    add select, fix buttons (#1)
    * add dist dir
    
    * fix button spinner
    
    * update dist
    
    * update readme, fix button spinning
    
    * feat(devwatch): start rollup -w
    
    * fix button
    
    * update readme, clean up button
    
    * changed main in package.json
    
    * updated build config
    
    * created select box, refactored text input
    
    * fixed untracked files
    
    * remove extra rollup config

---
+++ devwatch.js
@@ -0,0 +1,16 @@
+module.exports = {
+  watchers: function(Monitor) {
+    return [
+      new Monitor('start', {
+        command: '/usr/local/bin/npm run',
+        cwd: '/react-spark',
+        logFile: '/log/react-spark.log',
+        killSignal: 'SIGTERM',
+        minUptime: 3000,
+        spinSleepTime: 15000,
+        silent: false,
+        watch: false
+      })
+    ]
+  }
+}
--- rollup.config.js
+++ rollup.config.js
@@ -15,11 +15,6 @@ export default {
       file: pkg.main,
       format: 'cjs',
       sourcemap: true
-    },
-    {
-      file: pkg.module,
-      format: 'es',
-      sourcemap: true
     }
   ],
   plugins: [
@@ -31,9 +26,16 @@ export default {
     svgr(),
     babel({
       exclude: 'node_modules/**',
-      plugins: [ 'external-helpers' ]
+      plugins: ['external-helpers']
     }),
     resolve(),
     commonjs()
-  ]
+  ],
+  watch: {
+    chokidar: {
+      usePolling: true,
+      interval: 1000
+    },
+    include: 'src/**/*'
+  }
 }
