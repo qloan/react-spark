532d99748c124be1ce5509014e39ada1f8c827d8
Wash (Secondary) <twash-@c02p115ug3qd.mi.corp.rockfin.com>
2018-11-28 19:04:26 -0500

    fixups

--- src/components/TextInput/TextInput.js
+++ src/components/TextInput/TextInput.js
@@ -8,15 +8,8 @@ import React from 'react'
 
 import InputContainer from './../InputContainer/InputContainer'
 
-<<<<<<< HEAD
 import { sparkClassName, sparkWidthClassName } from '../../util'
 import TEXTINPUT_VARIANTS from './variants'
-=======
-import {
-  sparkBaseClassName,
-  sparkWidthClassName
-} from '../../util'
->>>>>>> feat/redesign-phase1
 
 class TextInput extends React.Component {
   static defaultProps = {
@@ -57,47 +50,27 @@ class TextInput extends React.Component {
   }
 
   get className() {
-<<<<<<< HEAD
-    const {error, width, value, helper, variant} = this.props
+    const {className, error, width, value, helper, variant} = this.props
 
     const baseClass = sparkClassName('base', 'TextInput')
     const errorClass = sparkClassName('base', 'TextInput', null, 'error')
     const helperClass = sparkClassName('base', 'TextInput', null, 'helper')
     const variantClass = sparkClassName('base', 'TextInput', null, variant)
-=======
-    const {className, error, width} = this.props
-
-    const errorClass = sparkBaseClassName('TextInput', null, 'error')
->>>>>>> feat/redesign-phase1
     const widthClass = sparkWidthClassName(width)
     const hasValueClass = value ? sparkClassName('base', 'TextInput', null, 'has-value') : null
 
-<<<<<<< HEAD
     return classNames(baseClass, {
       [errorClass]: error,
       [helperClass]: helper,
       [widthClass]: width,
       [variantClass]: variant,
-      [hasValueClass]: hasValueClass
+      [hasValueClass]: hasValueClass,
+      [className]: className
     })
   }
 
   render = () => {
-    const {disabled, error, helper, id, label, type, variant, placeholder, pattern, ...rest} = this.props
-=======
-    return classNames(
-      sparkBaseClassName('TextInput'),
-      {
-        [errorClass]: error,
-        [widthClass]: width,
-        [className]: className
-      }
-    )
-  }
-
-  render = () => {
-    const {className, disabled, error, id, label, type, ...props} = this.props
->>>>>>> feat/redesign-phase1
+    const {className, disabled, error, helper, id, label, type, variant, placeholder, pattern, ...props} = this.props
 
     return (
       <InputContainer error={error} helper={helper} id={id} label={label} variant={variant}>
@@ -109,14 +82,10 @@ class TextInput extends React.Component {
           type={type}
           aria-describedby={`${id}--error-container`}
           ref={this.inputRef}
-<<<<<<< HEAD
           placeholder={placeholder || this.variant.placeholder}
           pattern={pattern || this.variant.pattern}
           data-sprk-input={'text'}
-          {...rest}
-=======
           {...props}
->>>>>>> feat/redesign-phase1
         />
       </InputContainer>
     )
