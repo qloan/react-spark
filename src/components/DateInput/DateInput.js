fbd9f8c2e5893f6bc49d3fef486f094395f9d909
Patrik Csak <patrikcsak@rocketloans.com>
2018-12-06 10:32:20 -0500

    Add error support to date inputs

--- src/components/DateInput/DateInput.js
+++ src/components/DateInput/DateInput.js
@@ -7,12 +7,14 @@ import React from 'react'
 import {
   sparkBaseClassName,
   sparkClassName,
+  sparkComponentClassName,
   sparkWidthClassName
 } from '../../util'
 
 class DateInput extends React.Component {
   static defaultProps = {
     disabled: false,
+    error: null,
     pattern: '^(((0[1358]|1[02])([\\/-]?)(0[1-9]|[12]\\d|3[01])|(0[469]|11)([\\/-]?)(0[1-9]|[12]\\d|30)|02(\\/?)((0?\\d)|[12]\\d))(\\4|\\7|\\9)[12]\\d{3})?$',
     placeholder: 'MM/DD/YYYY',
     width: null
@@ -24,6 +26,7 @@ class DateInput extends React.Component {
 
   static propTypes = {
     disabled: PropTypes.bool,
+    error: PropTypes.string,
     id: PropTypes.string.isRequired,
     label: PropTypes.string.isRequired,
     pattern: PropTypes.string,
@@ -37,20 +40,76 @@ class DateInput extends React.Component {
   }
 
   get className() {
-    const {className, width} = this.props
+    const {className, error, width} = this.props
+    const errorClassName = error
+      ? sparkBaseClassName('TextInput', null, 'error')
+      : null
     const widthClassName = width ? sparkWidthClassName(width) : null
 
     return classnames(
       sparkBaseClassName('TextInput'),
-      {[widthClassName]: widthClassName},
-      {[className]: className}
+      {
+        [errorClassName]: errorClassName,
+        [widthClassName]: widthClassName,
+        [className]: className
+      }
     )
   }
+
+  get exclamationSvgClassName() {
+    return [
+      sparkComponentClassName('Icon'),
+      sparkComponentClassName('Icon', null, 'm'),
+      sparkBaseClassName('ErrorIcon')
+    ].join(' ')
+  }
+
+  renderErrorContent = () => {
+    const {error} = this.props
+
+    if (!error) return null
+
+    return (
+      <React.Fragment>
+        {this.renderExclamationSvg()}
+        <div className={sparkBaseClassName('ErrorText')}>{error}</div>
+      </React.Fragment>
+    )
+  }
+
+  renderExclamationSvg = () => (
+    <svg
+      className={this.exclamationSvgClassName}
+      height='100%'
+      viewBox='0 0 64 64'
+      width='100%'
+      xmlns='http://www.w3.org/2000/svg'
+    >
+      <g fillRule='evenodd' transform='translate(1 1)'>
+        <circle
+          className='exclamation-filled-circle'
+          cx='27'
+          cy='27'
+          r='27'
+        />
+        <g transform='translate(26 13)'>
+          <circle
+            className='exclamation-filled-i-dot'
+            cx='1'
+            cy='27.5'
+            r='1'
+          />
+          <path className='exclamation-filled-i-path' d='M0 0h2v22H0z' />
+        </g>
+      </g>
+    </svg>
+  )
 
   render = () => {
     const {
       className,
       disabled,
+      error,
       id,
       label,
       pattern,
@@ -67,6 +126,7 @@ class DateInput extends React.Component {
         >
           <input
             aria-describedby={`${id}--error-container`}
+            aria-invalid={!!error}
             className={this.className}
             data-id={id}
             disabled={disabled}
@@ -88,7 +148,9 @@ class DateInput extends React.Component {
           <div
             className={sparkBaseClassName('ErrorContainer')}
             id={`${id}-error-container`}
-          />
+          >
+            {this.renderErrorContent()}
+          </div>
         </div>
       </div>
     )
--- src/components/DatePicker/DatePicker.js
+++ src/components/DatePicker/DatePicker.js
@@ -13,6 +13,7 @@ import {
 class DatePicker extends React.Component {
   static defaultProps = {
     disabled: false,
+    error: null,
     pattern: '^(((0[1358]|1[02])([\\/-]?)(0[1-9]|[12]\\d|3[01])|(0[469]|11)([\\/-]?)(0[1-9]|[12]\\d|30)|02(\\/?)((0?\\d)|[12]\\d))(\\4|\\7|\\9)[12]\\d{3})?$',
     placeholder: 'MM/DD/YYYY',
     width: null
@@ -24,6 +25,7 @@ class DatePicker extends React.Component {
 
   static propTypes = {
     disabled: PropTypes.bool,
+    error: PropTypes.string,
     id: PropTypes.string.isRequired,
     label: PropTypes.string.isRequired,
     pattern: PropTypes.string,
@@ -38,7 +40,10 @@ class DatePicker extends React.Component {
   }
 
   get className() {
-    const {className, width} = this.props
+    const {className, error, width} = this.props
+    const errorClassName = error
+      ? sparkBaseClassName('TextInput', null, 'error')
+      : null
     const widthClassName = width ? sparkWidthClassName(width) : null
 
     return classnames(
@@ -46,8 +51,11 @@ class DatePicker extends React.Component {
       sparkBaseClassName('TextInput', null, 'with-icon'),
       sparkClassName('utility', 'pll'),
       sparkWidthClassName(100),
-      {[widthClassName]: widthClassName},
-      {[className]: className}
+      {
+        [errorClassName]: errorClassName,
+        [widthClassName]: widthClassName,
+        [className]: className
+      }
     )
   }
 
@@ -56,6 +64,14 @@ class DatePicker extends React.Component {
       sparkComponentClassName('Icon'),
       sparkComponentClassName('Icon', null, 'current-color'),
       sparkBaseClassName('DatePicker', 'icon')
+    ].join(' ')
+  }
+
+  get exclamationSvgClassName() {
+    return [
+      sparkComponentClassName('Icon'),
+      sparkComponentClassName('Icon', null, 'm'),
+      sparkBaseClassName('ErrorIcon')
     ].join(' ')
   }
 
@@ -73,10 +89,52 @@ class DatePicker extends React.Component {
     </svg>
   )
 
+  renderErrorContent = () => {
+    const {error} = this.props
+
+    if (!error) return null
+
+    return (
+      <React.Fragment>
+        {this.renderExclamationSvg()}
+        <div className={sparkBaseClassName('ErrorText')}>{error}</div>
+      </React.Fragment>
+    )
+  }
+
+  renderExclamationSvg = () => (
+    <svg
+      className={this.exclamationSvgClassName}
+      height='100%'
+      viewBox='0 0 64 64'
+      width='100%'
+      xmlns='http://www.w3.org/2000/svg'
+    >
+      <g fillRule='evenodd' transform='translate(1 1)'>
+        <circle
+          className='exclamation-filled-circle'
+          cx='27'
+          cy='27'
+          r='27'
+        />
+        <g transform='translate(26 13)'>
+          <circle
+            className='exclamation-filled-i-dot'
+            cx='1'
+            cy='27.5'
+            r='1'
+          />
+          <path className='exclamation-filled-i-path' d='M0 0h2v22H0z' />
+        </g>
+      </g>
+    </svg>
+  )
+
   render = () => {
     const {
       className,
       disabled,
+      error,
       id,
       label,
       pattern,
@@ -94,6 +152,7 @@ class DatePicker extends React.Component {
           {this.renderCalendarSvg()}
           <input
             aria-describedby={`${id}--error-container`}
+            aria-invalid={!!error}
             className={this.className}
             data-id={id}
             disabled={disabled}
@@ -116,7 +175,9 @@ class DatePicker extends React.Component {
         <div
           className={sparkBaseClassName('ErrorContainer')}
           id={`${id}-error-container`}
-        />
+        >
+          {this.renderErrorContent()}
+        </div>
       </div>
     )
   }
