68887562441079503ea79b32afe107c5893a5ed7
Patrik Csak <patrikcsak@rocketloans.com>
2018-12-09 14:22:39 -0500

    Add table of contents

--- example/src/components/examples/Inputs.js
+++ example/src/components/examples/Inputs.js
@@ -3,6 +3,9 @@ import {
   CheckboxGroup,
   DateInput,
   DatePicker,
+  Divider,
+  Link,
+  List,
   MonetaryInput,
   Select,
   SsnInput,
@@ -40,142 +43,148 @@ class Inputs extends React.Component {
     this.setState({textInputWithValueValue: event.target.value})
   }
 
-  renderCheckboxes = () => (<>
-    <h3>Checkbox Group</h3>
-    <p>Normal checkbox group:</p>
-    <CheckboxGroup
-      checkboxes={[
-        {id: 'checkbox-normal-1', label: 'Checkbox Item 1'},
-        {id: 'checkbox-normal-2', label: 'Checkbox Item 2'},
-        {id: 'checkbox-normal-3', label: 'Checkbox Item 3'}
-      ]}
-      className='extra-class'
-      data-extra-attribute
-      id={'checkbox-normal'}
-      label='Checkbox Group Label'
-    />
+  renderCheckboxes = () => (
+    <div id='checkboxes'>
+      <h3>Checkbox Group</h3>
+      <p>Normal checkbox group:</p>
+      <CheckboxGroup
+        checkboxes={[
+          {id: 'checkbox-normal-1', label: 'Checkbox Item 1'},
+          {id: 'checkbox-normal-2', label: 'Checkbox Item 2'},
+          {id: 'checkbox-normal-3', label: 'Checkbox Item 3'}
+        ]}
+        className='extra-class'
+        data-extra-attribute
+        id={'checkbox-normal'}
+        label='Checkbox Group Label'
+      />
 
-    <p>Checkbox group with error:</p>
-    <CheckboxGroup
-      checkboxes={[
-        {id: 'checkbox-error-1', label: 'Checkbox Item 1'},
-        {id: 'checkbox-error-2', label: 'Checkbox Item 2'},
-        {id: 'checkbox-error-3', label: 'Checkbox Item 3'}
-      ]}
-      error='There is an error on this field.'
-      id={'checkbox-error'}
-      label='Checkbox Group Label'
-    />
+      <p>Checkbox group with error:</p>
+      <CheckboxGroup
+        checkboxes={[
+          {id: 'checkbox-error-1', label: 'Checkbox Item 1'},
+          {id: 'checkbox-error-2', label: 'Checkbox Item 2'},
+          {id: 'checkbox-error-3', label: 'Checkbox Item 3'}
+        ]}
+        error='There is an error on this field.'
+        id={'checkbox-error'}
+        label='Checkbox Group Label'
+      />
 
-    <p>Disabled checkbox group:</p>
-    <CheckboxGroup
-      checkboxes={[
-        {id: 'checkbox-disabled-1', label: 'Checkbox Item 1'},
-        {id: 'checkbox-disabled-2', label: 'Checkbox Item 2'},
-        {id: 'checkbox-disabled-3', label: 'Checkbox Item 3'}
-      ]}
-      disabled
-      id={'checkbox-disabled'}
-      label='Checkbox Group Label'
-    />
-  </>)
+      <p>Disabled checkbox group:</p>
+      <CheckboxGroup
+        checkboxes={[
+          {id: 'checkbox-disabled-1', label: 'Checkbox Item 1'},
+          {id: 'checkbox-disabled-2', label: 'Checkbox Item 2'},
+          {id: 'checkbox-disabled-3', label: 'Checkbox Item 3'}
+        ]}
+        disabled
+        id={'checkbox-disabled'}
+        label='Checkbox Group Label'
+      />
+    </div>
+  )
 
-  renderDateInputs = () => (<>
-    <h3>Date Inputs</h3>
+  renderDateInputs = () => (
+    <div id='date-inputs'>
+      <h3>Date Inputs</h3>
 
-    <p>Normal:</p>
-    <DateInput
-      className='extra-class'
-      data-extra-attribute
-      id='date-input-normal'
-      label='Date'
-      onChange={this.handleDateInputChange}
-      value={this.state.dateInputValue}
-      width={100}
-    />
+      <p>Normal:</p>
+      <DateInput
+        className='extra-class'
+        data-extra-attribute
+        id='date-input-normal'
+        label='Date'
+        onChange={this.handleDateInputChange}
+        value={this.state.dateInputValue}
+        width={100}
+      />
 
-    <p>Error:</p>
-    <DateInput
-      className='extra-class'
-      data-extra-attribute
-      error='There is an error on this field.'
-      id='date-input-error'
-      label='Date'
-      width={100}
-    />
+      <p>Error:</p>
+      <DateInput
+        className='extra-class'
+        data-extra-attribute
+        error='There is an error on this field.'
+        id='date-input-error'
+        label='Date'
+        width={100}
+      />
 
-    <p>Disabled:</p>
-    <DateInput
-      disabled
-      id='date-input-disabled'
-      label='Date'
-      width={100}
-    />
+      <p>Disabled:</p>
+      <DateInput
+        disabled
+        id='date-input-disabled'
+        label='Date'
+        width={100}
+      />
 
-    <h3>Date Picker</h3>
+      <h3>Date Picker</h3>
 
-    <Type.BodyOne>
-      WARNING: Input/change events don't yet work
-      on <code>&lt;DatePicker&gt;</code>.
-    </Type.BodyOne>
+      <Type.BodyOne>
+        WARNING: Input/change events don't yet work
+        on <code>&lt;DatePicker&gt;</code>.
+      </Type.BodyOne>
 
-    <p>Normal:</p>
-    <DatePicker
-      className='extra-class'
-      data-extra-attribute
-      id='date-picker-normal'
-      label='Date'
-      onChange={this.handleDatePickerChange}
-      value={this.state.datePickerValue}
-      width={100}
-    />
+      <p>Normal:</p>
+      <DatePicker
+        className='extra-class'
+        data-extra-attribute
+        id='date-picker-normal'
+        label='Date'
+        onChange={this.handleDatePickerChange}
+        value={this.state.datePickerValue}
+        width={100}
+      />
 
-    <p>Error:</p>
-    <DatePicker
-      className='extra-class'
-      data-extra-attribute
-      error='There is an error on this field.'
-      id='date-picker-error'
-      label='Date'
-      width={100}
-    />
+      <p>Error:</p>
+      <DatePicker
+        className='extra-class'
+        data-extra-attribute
+        error='There is an error on this field.'
+        id='date-picker-error'
+        label='Date'
+        width={100}
+      />
 
-    <p>Disabled:</p>
-    <DatePicker
-      disabled
-      id='date-picker-disabled'
-      label='Date'
-      width={100}
-    />
-  </>)
+      <p>Disabled:</p>
+      <DatePicker
+        disabled
+        id='date-picker-disabled'
+        label='Date'
+        width={100}
+      />
+    </div>
+  )
 
-  renderMonetaryInputs = () => (<>
-    <h3>Monetary Input</h3>
+  renderMonetaryInputs = () => (
+    <div id='monetary-inputs'>
+      <h3>Monetary Input</h3>
 
-    <p>Normal monetary input:</p>
-    <MonetaryInput
-      className='extra-class'
-      data-extra-attribute
-      id='monetary-normal'
-      label='Payment'
-      onChange={this.handleMonetaryInputChange}
-      value={this.state.monetaryInputValue}
-    />
+      <p>Normal monetary input:</p>
+      <MonetaryInput
+        className='extra-class'
+        data-extra-attribute
+        id='monetary-normal'
+        label='Payment'
+        onChange={this.handleMonetaryInputChange}
+        value={this.state.monetaryInputValue}
+      />
 
-    <p>Monetary input with error:</p>
-    <MonetaryInput
-      error='There is an error on this field.'
-      id='monetary-error'
-      label='Payment'
-    />
+      <p>Monetary input with error:</p>
+      <MonetaryInput
+        error='There is an error on this field.'
+        id='monetary-error'
+        label='Payment'
+      />
 
-    <p>Disabled monetary input:</p>
-    <MonetaryInput
-      disabled
-      id='monetary-disabled'
-      label='Payment'
-    />
-  </>)
+      <p>Disabled monetary input:</p>
+      <MonetaryInput
+        disabled
+        id='monetary-disabled'
+        label='Payment'
+      />
+    </div>
+  )
 
   renderSelects = () => {
     const options = [
@@ -183,124 +192,150 @@ class Inputs extends React.Component {
       {value: 34, text: 'Second val'}
     ]
 
-    return (<>
-      <h3>Select:</h3>
+    return (
+      <div id='selects'>
+        <h3>Select:</h3>
 
-      <Select
-        className='extra-class'
-        data-extra-attribute
-        id='select-normal'
-        label='Select Label'
-        options={options}
-        width={100}
-      />
-    </>)
+        <Select
+          className='extra-class'
+          data-extra-attribute
+          id='select-normal'
+          label='Select Label'
+          options={options}
+          width={100}
+        />
+      </div>
+    )
   }
 
-  renderSsns = () => (<>
-    <h3>SSN Inputs</h3>
+  renderSsns = () => (
+    <div id='ssn-inputs'>
+      <h3>SSN Inputs</h3>
 
-    <p>Normal SSN input:</p>
-    <SsnInput
-      className='extra-class'
-      data-extra-attribute
-      id='ssn-normal'
-      onChange={this.handleSsnInputChange}
-      value={this.state.ssnInputValue}
-    />
+      <p>Normal SSN input:</p>
+      <SsnInput
+        className='extra-class'
+        data-extra-attribute
+        id='ssn-normal'
+        onChange={this.handleSsnInputChange}
+        value={this.state.ssnInputValue}
+      />
 
-    <p>SSN input with error:</p>
-    <SsnInput
-      className='extra-class'
-      data-extra-attribute
-      error='There is an error in this field.'
-      id='ssn-error'
-    />
+      <p>SSN input with error:</p>
+      <SsnInput
+        className='extra-class'
+        data-extra-attribute
+        error='There is an error in this field.'
+        id='ssn-error'
+      />
 
-    <p>Disabled SSN input:</p>
-    <SsnInput
-      className='extra-class'
-      data-extra-attribute
-      disabled
-      id='ssn-disabled'
-    />
-  </>)
+      <p>Disabled SSN input:</p>
+      <SsnInput
+        className='extra-class'
+        data-extra-attribute
+        disabled
+        id='ssn-disabled'
+      />
+    </div>
+  )
 
-  renderTextInputs = () => (<>
-    <h3>Text inputs</h3>
+  renderTextInputs = () => (
+    <div id='text-inputs'>
+      <h3>Text inputs</h3>
 
-    <p>Text input:</p>
-    <TextInput
-      className='extra-class'
-      data-extra-attribute
-      id='text-input-normal'
-      label='Text Input Label'
-      width={100}
-    />
+      <p>Text input:</p>
+      <TextInput
+        className='extra-class'
+        data-extra-attribute
+        id='text-input-normal'
+        label='Text Input Label'
+        width={100}
+      />
 
-    <p>Text input with value:</p>
-    <TextInput
-      id='text-input-with-value'
-      label='Text Input Label'
-      onChange={this.handleTextInputChange}
-      value={this.state.textInputWithValueValue}
-      width={100}
-    />
+      <p>Text input with value:</p>
+      <TextInput
+        id='text-input-with-value'
+        label='Text Input Label'
+        onChange={this.handleTextInputChange}
+        value={this.state.textInputWithValueValue}
+        width={100}
+      />
 
-    <p>Text input with error:</p>
-    <TextInput
-      error={'There is an error on this field.'}
-      id='text-input-error'
-      label='Text Input Label'
-      width={100}
-    />
+      <p>Text input with error:</p>
+      <TextInput
+        error={'There is an error on this field.'}
+        id='text-input-error'
+        label='Text Input Label'
+        width={100}
+      />
 
-    <p>Text input with helper text:</p>
-    <TextInput
-      helper={'This is helper text for this input.'}
-      id='text-input-helper'
-      label='Text Input Label'
-      width={100}
-    />
+      <p>Text input with helper text:</p>
+      <TextInput
+        helper={'This is helper text for this input.'}
+        id='text-input-helper'
+        label='Text Input Label'
+        width={100}
+      />
 
-    <p>Disabled text input:</p>
-    <TextInput
-      disabled
-      id='text-input-disabled'
-      label='Text Input Label'
-      width={100}
-    />
-  </>)
+      <p>Disabled text input:</p>
+      <TextInput
+        disabled
+        id='text-input-disabled'
+        label='Text Input Label'
+        width={100}
+      />
+    </div>
+  )
 
-  renderTextAreas = () => (<>
-    <h3>Text Areas</h3>
+  renderTextAreas = () => (
+    <div id='text-areas'>
+      <h3>Text Areas</h3>
 
-    <p>Text area:</p>
-    <TextArea id='text-input-normal' label='TextArea Label' width={100} />
+      <p>Text area:</p>
+      <TextArea id='text-input-normal' label='TextArea Label' width={100} />
 
-    <p>Text input with error:</p>
-    <TextArea
-      className='extra-class'
-      data-extra-attribute
-      error={'There is an error on this field.'}
-      id='text-input-error'
-      label='TextArea Label'
-      width={100}
-    />
+      <p>Text input with error:</p>
+      <TextArea
+        className='extra-class'
+        data-extra-attribute
+        error={'There is an error on this field.'}
+        id='text-input-error'
+        label='TextArea Label'
+        width={100}
+      />
 
-    <p>Disabled text area:</p>
-    <TextArea
-      disabled
-      id='text-input-disabled'
-      label='TextArea Label'
-      width={100}
-    />
-  </>)
+      <p>Disabled text area:</p>
+      <TextArea
+        disabled
+        id='text-input-disabled'
+        label='TextArea Label'
+        width={100}
+      />
+    </div>
+  )
+
+  renderToc = () => (<>
+      <h3>Table of Contents</h3>
+
+      <List element='ul'>
+        <li><Link href='#text-inputs'>Text inputs</Link></li>
+        <li><Link href='#checkboxes'>Checkboxes</Link></li>
+        <li><Link href='#selects'>Selects</Link></li>
+        <li><Link href='#text-areas'>Text areas</Link></li>
+        <li><Link href='#ssn-inputs'>SSN inputs</Link></li>
+        <li><Link href='#monetary-inputs'>Monetary inputs</Link></li>
+        <li><Link href='#date-inputs'>Date inputs</Link></li>
+      </List>
+    </>
+  )
 
   render = () => (<>
     <h2>Inputs</h2>
 
-    {/* TODO: Add table of contents */}
+    <Divider />
+    {this.renderToc()}
+    <Divider />
+
     {this.renderTextInputs()}
     {this.renderCheckboxes()}
     {this.renderSelects()}
