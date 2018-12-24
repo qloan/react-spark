ff46c2a7f1e17a1f8e787b5fee147ac84b7450ff
iaghidro <steveghidro@quickenloans.com>
2018-12-24 15:20:38 -0500

    fix footer color

--- src/components/Box/Box.js
+++ src/components/Box/Box.js
@@ -1,46 +1,50 @@
-import classNames from 'classnames'
-import PropTypes from 'prop-types'
-import React from 'react'
+import classNames from "classnames";
+import PropTypes from "prop-types";
+import React from "react";
 
-import { sparkObjectClassName, sparkClassName } from '../../util'
-import PADDING_SIZES from './paddingSizes'
+import { sparkObjectClassName, sparkClassName } from "../../util";
+import PADDING_SIZES from "./paddingSizes";
 
 class Box extends React.Component {
   static defaultProps = {
     children: null,
     padding: null
-  }
+  };
 
   static propTypes = {
     children: PropTypes.node,
     padding: PropTypes.oneOf(Object.values(PADDING_SIZES)),
     color: PropTypes.string
-  }
+  };
 
   get className() {
-    const {className, color, padding} = this.props
-    const block = 'Box'
-    const paddingSizeClassName = sparkObjectClassName(block, null, padding)
+    const { className, color, padding } = this.props;
+    const block = "Box";
+    const paddingSizeClassName = sparkObjectClassName(block, null, padding);
     const colorClassName = sparkClassName(
-      'utility',
-      'BackgroundColor',
+      "utility",
+      "BackgroundColor",
       null,
-      `${color}-tint-50`
-    )
+      `${color}`
+    );
 
     return classNames(
       sparkObjectClassName(block),
-      {[paddingSizeClassName]: padding},
-      {[colorClassName]: Boolean(color)},
-      {[className]: className}
-    )
+      { [paddingSizeClassName]: padding },
+      { [colorClassName]: Boolean(color) },
+      { [className]: className }
+    );
   }
 
   render = () => {
-    const {children, className, ...props} = this.props
+    const { children, className, ...props } = this.props;
 
-    return <div className={this.className} {...props}>{children}</div>
-  }
+    return (
+      <div className={this.className} {...props}>
+        {children}
+      </div>
+    );
+  };
 }
 
-export default Box
+export default Box;
