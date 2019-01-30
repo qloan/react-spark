9554f8fdb3642a0fc56283f61ed6790a542b1f69
iaghidro <steveghidro@quickenloans.com>
2019-01-30 15:13:44 -0500

    fix ie bug

--- src/util/breakpointWordToLetter.js
+++ src/util/breakpointWordToLetter.js
@@ -1,16 +1,16 @@
-const dictionary = new Map([
-  ['min', 'xxs'],
-  ['tiny', 'xs'],
-  ['small', 's'],
-  ['medium', 'm'],
-  ['large', 'l'],
-  ['huge', 'xl']
-])
+const dictionary = {
+  min: 'xxs',
+  tiny: 'xs',
+  small: 's',
+  medium: 'm',
+  large: 'l',
+  huge: 'xl'
+}
 
 /**
  * @param {string} word
  * @returns {?string}
  */
-const breakpointWordToLetter = word => dictionary.get(word) || null
+const breakpointWordToLetter = word => dictionary[word] || null
 
 export default breakpointWordToLetter
