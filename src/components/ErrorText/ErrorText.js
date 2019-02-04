f50e87b9b652bdb88ce212fdd37ffe83c15aba92
iaghidro <steveghidro@quickenloans.com>
2019-02-04 11:56:11 -0500

    fix text input validation

--- src/components/ErrorText/ErrorText.js
+++ src/components/ErrorText/ErrorText.js
@@ -1,31 +1,17 @@
-import React, { Fragment } from 'react';
-import Icon from '../Icon';
-import { sparkBaseClassName } from '../../util';
+import React from 'react'
+import { sparkBaseClassName } from '../../util'
 
 class ErrorText extends React.Component {
   render = () => {
     const { id, error } = this.props
 
     return (
-      error && (
-        <div
-          // todo: add this back if possible
-          // className={sparkBaseClassName('ErrorContainer')}
-          // spark code is currently using sprk-b-ErrorContainer class to remove html, and this breaks validation
-          // this styling is copied from the sprk-b-ErrorContainer class
-          style={{
-            msFlexAlign: 'center',
-            alignItems: 'center',
-            display: '-ms-flexbox',
-            display: 'flex',
-            marginTop: '8px'
-          }}
-          id={`${id}--error-container`}
-        >
-          <Icon name='exclamation-filled' className='sprk-b-ErrorIcon' />
-          <div className={sparkBaseClassName('ErrorText')}>{error}</div>
-        </div>
-      )
+      <div
+        className={sparkBaseClassName('ErrorContainer')}
+        id={`${id}--error-container`}
+      >
+        <div className={sparkBaseClassName('ErrorText')}>{error}</div>
+      </div>
     )
   };
 }
