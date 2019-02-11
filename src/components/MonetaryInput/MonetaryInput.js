5e2fb139434abe31110666605ffaa6d00ba1ccc9
Istvan (Steve) Ghidro <steveghidro@quickenloans.com>
2019-02-11 12:04:47 -0500

    Revert "Revert "add pattern to monetary input"" (#33)

--- src/components/MonetaryInput/MonetaryInput.js
+++ src/components/MonetaryInput/MonetaryInput.js
@@ -1,13 +1,14 @@
 import {
   bindMonetaryUIEvents,
   formatMonetary
-} from '@sparkdesignsystem/spark-core'
-import classnames from 'classnames'
-import PropTypes from 'prop-types'
-import React from 'react'
-import InputContainer from './../InputContainer/InputContainer'
-import { sparkBaseClassName, sparkClassName } from '../../util'
+} from '@sparkdesignsystem/spark-core';
+import classnames from 'classnames';
+import PropTypes from 'prop-types';
+import React from 'react';
+import InputContainer from './../InputContainer/InputContainer';
+import { sparkBaseClassName, sparkClassName } from '../../util';
 
+export const pattern = /(^\$?(\d+|\d{1,3}(,\d{3})*)(\.\d+)?$)|^$/
 class MonetaryInput extends React.Component {
   static defaultProps = {
     className: null,
@@ -166,5 +167,7 @@ class MonetaryInput extends React.Component {
     )
   };
 }
+
+MonetaryInput.pattern = pattern
 
 export default MonetaryInput
