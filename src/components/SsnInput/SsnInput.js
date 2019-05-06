cc8c41c47938a34ff39d8cae1e6a8e523511d62d
Shathar Campbell <shatharcampbell@gmail.com>
2019-05-06 14:45:54 -0400

    cleaned up SSnInput

--- src/components/SsnInput/SsnInput.js
+++ src/components/SsnInput/SsnInput.js
@@ -1,9 +1,6 @@
 import { SprkRevealInput } from '@sparkdesignsystem/spark-react'
 import React from 'react'
-import {
-  // isValidSSN as sparkIsValidSSN,
-  formatSSN,
-} from '@sparkdesignsystem/spark-core';
+import {formatSSN} from '@sparkdesignsystem/spark-core';
 import PropTypes from 'prop-types';
 class SsnInput extends React.Component {
 
@@ -12,7 +9,6 @@ class SsnInput extends React.Component {
         disabled: false,
         error: null,
         label: 'Social Security Number',
-        // pattern: null,
         placeholder: '000-00-0000',
         value: '',
         width: 100,
@@ -26,41 +22,11 @@ class SsnInput extends React.Component {
         id: PropTypes.string.isRequired,
         label: PropTypes.string,
         onChange: PropTypes.func,
-        // pattern: PropTypes.string,
         placeholder: PropTypes.string,
         value: PropTypes.string,
         helper: PropTypes.string,
         width: PropTypes.number,
       };
-
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {
-  //     ssn: '',
-  //   };
-  //   this.handleChange = this.handleChange.bind(this);
-  // }
-  
-  // isValidSSN(ssn){
-  //   console.log(ssn);
-  //   const {pattern} = this.props;
-  //   if (pattern)
-  //   {
-  //     return new RegExp(pattern).test(ssn);
-  //   }
-
-  //   const sparkValidSSN = sparkIsValidSSN(ssn);
-   
-  //   return sparkValidSSN;
-  // }
-
-  // handleChange({ target }) {
-  //   const { name, value } = target;
-  //   this.setState({
-  //     ssn: value,
-  //   });
-  // }
-
 
     handleChange = event => {
     event.target.value = event.target.value.replace(/-/g, '')
@@ -68,7 +34,6 @@ class SsnInput extends React.Component {
       this.props.onChange(event)
     }
   };
-
 
   render(){
   const {
@@ -80,7 +45,6 @@ class SsnInput extends React.Component {
     value,
     ...rest
   } = this.props;
-  // const { ssn } = this.state;
 
   return(
     <SprkRevealInput
@@ -100,164 +64,3 @@ class SsnInput extends React.Component {
 }
 }
 export default SsnInput
-
-
-
-
-
-
-// import { bindSSNInputUIEvents, formatSSN } from '@sparkdesignsystem/spark-core'
-// import classnames from 'classnames'
-// import PropTypes from 'prop-types'
-// import React from 'react'
-// import InputContainer from './../InputContainer/InputContainer'
-// import { sparkBaseClassName, sparkClassName } from '../../util'
-
-// export const ssnInputValidationRegex =
-//   '(^(?!666|000|9\\d{2})\\d{3}([-]{0,1})(?!00)\\d{2}\\1(?!0{4})\\2\\d{4}$)|^$'
-// class SsnInput extends React.Component {
-//   static defaultProps = {
-//     className: null,
-//     disabled: false,
-//     error: null,
-//     label: 'Social Security Number',
-//     pattern: ssnInputValidationRegex,
-//     placeholder: '000-00-0000',
-//     showSsnLabel: 'Show Social Security Number',
-//     value: '',
-//     width: 100
-//   };
-
-//   inputRef = React.createRef();
-
-//   static propTypes = {
-//     className: PropTypes.string,
-//     disabled: PropTypes.bool,
-//     error: PropTypes.string,
-//     id: PropTypes.string.isRequired,
-//     label: PropTypes.string,
-//     pattern: PropTypes.string,
-//     placeholder: PropTypes.string,
-//     showSsnLabel: PropTypes.string,
-//     value: PropTypes.string,
-//     helper: PropTypes.string,
-//     width: PropTypes.number
-//   };
-
-//   get className() {
-//     const { className, error, width } = this.props
-//     const errorClassName = error
-//       ? sparkBaseClassName('TextInput', null, 'error')
-//       : null
-//     const widthClassName = width
-//       ? sparkClassName('utility', `Width-${width}`)
-//       : null
-
-//     return classnames(sparkBaseClassName('TextInput'), {
-//       [errorClassName]: errorClassName,
-//       [widthClassName]: widthClassName,
-//       [className]: className
-//     })
-//   }
-
-//   componentDidMount = () => {
-//     const inputElement = this.inputRef.current
-//     bindSSNInputUIEvents(inputElement)
-//   };
-
-//   handleChange = event => {
-//     event.target.value = event.target.value.replace(/-/g, '')
-//     if (typeof this.props.onChange === 'function') {
-//       this.props.onChange(event)
-//     }
-//   };
-
-//   handleBlur = event => {
-//     event.target.value = event.target.value.replace(/-/g, '')
-//     if (typeof this.props.onBlur === 'function') {
-//       this.props.onBlur(event)
-//     }
-//   };
-
-//   get maskedValue() {
-//     const { pattern, value } = this.props
-//     const inputElement = this.inputRef.current
-//     const patternRegex = new RegExp(pattern)
-
-//     if (value == null) {
-//       // This component is uncontrolled
-//       return patternRegex.test(inputElement.value)
-//         ? formatSSN(inputElement.value)
-//         : inputElement.value
-//     } else {
-//       return patternRegex.test(value) ? formatSSN(value) : value
-//     }
-//   }
-
-//   get selectionContainerClassName() {
-//     return [
-//       sparkBaseClassName('SelectionContainer'),
-//       sparkBaseClassName('InputContainer', 'visibility-toggle')
-//     ].join(' ')
-//   }
-
-//   get showSsnLabelClassName() {
-//     return [
-//       sparkBaseClassName('Label'),
-//       sparkBaseClassName('Label', null, 'inline')
-//     ].join(' ')
-//   }
-
-//   render = () => {
-//     const {
-//       className,
-//       disabled,
-//       error,
-//       id,
-//       label,
-//       pattern,
-//       placeholder,
-//       showSsnLabel,
-//       value,
-//       width,
-//       helper,
-//       ...props
-//     } = this.props
-//     const valueProp = value == null ? {} : { value: formatSSN(value) }
-//     return (
-//       <InputContainer
-//         error={error}
-//         helper={helper}
-//         id={id}
-//         label={label}
-//         inputRef={this.inputRef}
-//         data-sprk-input='ssn'
-//       >
-//         <input
-//           aria-describedby={`${id}--error-container`}
-//           className={this.className}
-//           disabled={disabled}
-//           id={id}
-//           pattern={pattern}
-//           placeholder={placeholder}
-//           {...valueProp}
-//           {...props}
-//           type='password'
-//           onChange={this.handleChange}
-//           onBlur={this.handleBlur}
-//         />
-//         <div className={this.selectionContainerClassName}>
-//           <input disabled={disabled} id={`${id}-show-ssn`} type='checkbox' />
-//           <label
-//             className={this.showSsnLabelClassName}
-//             htmlFor={`${id}-show-ssn`}
-//           >
-//             {showSsnLabel}
-//           </label>
-//         </div>
-//       </InputContainer>
-//     )
-//   };
-// }
-
-// export default SsnInput
