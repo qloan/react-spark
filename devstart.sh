7d8c7628820b66bb04154f4c97fc981dbc9b287b
Norman Witte III <normwitte@rocketloans.com>
2020-03-31 19:52:49 -0400

    modernize devstart bash script

--- devstart.sh
+++ devstart.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
-cd /react-spark/example;
 
-jq 'del(.dependencies["react-spark"])' package.json > package.json.tmp;
-mv package.json.tmp package.json;
-rm -rf node_modules/react-spark;
+pushd "${SOURCE_DIR}/react-spark/example";
 
-if [ "$1" = "clean" ]; then
-    echo "Removing node_modules folder"
-    rm -rf node_modules
-fi
+    cp package.json package.json.backup;
+    jq 'del(.dependencies["react-spark"])' package.json > package.json.tmp;
+    mv package.json.tmp package.json;
+    rm -rf node_modules/react-spark;
 
-mkdir -p node_modules;
-cd node_modules;
-npm link react-spark;
-npm install;
+    if [ "$1" = "clean" ]; then
+        echo "Removing node_modules folder"
+        rm -rf node_modules
+    fi
+
+    mkdir -p node_modules;
+
+    pushd "./node_modules";
+        npm link react-spark;
+        npm install;
+    popd;
+
+    mv package.json.backup package.json;
+
+popd;
