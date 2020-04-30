8ada75707905dee904bb17b2eb2cab20e1c2c603
TEkizian <tylerekizian@rocketloans.com>
2020-04-30 09:11:38 -0400

    Modify nav links to accept functions to generate text

--- src/components/Masthead/LittleNav/NavItem.js
+++ src/components/Masthead/LittleNav/NavItem.js
@@ -10,98 +10,98 @@ import { sparkComponentClassName, sparkBaseClassName, sparkClassName } from '../
 
 class NavItem extends React.Component {
   static propTypes = {
-    id: PropTypes.string.isRequired,
-    right: PropTypes.bool,
-    link: PropTypes.shape({
-      buttonVariant: PropTypes.oneOf(
-        Object.keys(BUTTON_VARIANTS).map(itm => BUTTON_VARIANTS[itm])
-      ),
-      href: PropTypes.string,
-      text: PropTypes.string,
-      variant: PropTypes.string,
-      target: PropTypes.string,
-      icon: PropTypes.string,
-      chevron: PropTypes.bool,
-      links: PropTypes.arrayOf(
-        PropTypes.shape({
-          text: PropTypes.string.isRequired,
+      id: PropTypes.string.isRequired,
+      right: PropTypes.bool,
+      link: PropTypes.shape({
+          buttonVariant: PropTypes.oneOf(
+              Object.keys(BUTTON_VARIANTS).map(itm => BUTTON_VARIANTS[itm])
+          ),
           href: PropTypes.string,
-          onClick: PropTypes.func
-        })
-      )
-    })
+          text: PropTypes.string,
+          variant: PropTypes.string,
+          target: PropTypes.string,
+          icon: PropTypes.string,
+          chevron: PropTypes.bool,
+          links: PropTypes.arrayOf(
+              PropTypes.shape({
+                  text: PropTypes.string.isRequired,
+                  href: PropTypes.string,
+                  onClick: PropTypes.func
+              })
+          )
+      })
   };
 
   get hasLinks() {
-    const { link } = this.props
-    return !!(link.links && link.links.length)
+      const { link } = this.props
+      return !!(link.links && link.links.length)
   }
 
   get linkClassName() {
-    return classnames(
-      sparkComponentClassName('Masthead', 'link'),
-      sparkBaseClassName('Link', null, 'plain')
-    )
+      return classnames(
+          sparkComponentClassName('Masthead', 'link'),
+          sparkBaseClassName('Link', null, 'plain')
+      )
   }
 
   render = () => {
-    const {
-      buttonVariant,
-      href,
-      text,
-      variant,
-      icon,
-      links,
-      target,
-      onClick,
-      chevron
-    } = this.props.link
-    const { id, right } = this.props
-    return (
-      <li>
-        {this.hasLinks ? (
-          <Fragment>
-            <Dropdown.DropdownLink
-              variant='plain'
-              href={href}
-              text={text}
-              icon={icon}
-              target={target}
-              id={id}
-              chevron={chevron}
-              className={sparkComponentClassName('Masthead', 'link')}
-            />
-            <Dropdown.DropdownLinksContainer id={id} className={right ? sparkClassName('utility', 'Right', null, 'zero') : ''}>
-              <Dropdown.DropdownLinks links={links} />
-            </Dropdown.DropdownLinksContainer>
-          </Fragment>
-        ) : (
-          <Fragment>
-            {buttonVariant ? (
-              <Button
-                href={href}
-                variant={buttonVariant}
-                onClick={onClick}
-                className={sparkComponentClassName('Button', null, 'compact')}
-              >
-                {text}
-              </Button>
-            ) : (
-              <Link
-                href={href}
-                variant={variant}
-                className={this.linkClassName}
-                target={target}
-                style={{ textDecoration: 'none' }}
-                onClick={onClick}
-              >
-                {text || <Icon name={icon} size={Icon.size.L} color='base' />}
-              </Link>
-            )}
-          </Fragment>
-        )}
-      </li>
-    )
+      const {
+          buttonVariant,
+          href,
+          text,
+          variant,
+          icon,
+          links,
+          target,
+          onClick,
+          chevron
+      } = this.props.link
+      const { id, right } = this.props
+      return (
+          <li>
+              {this.hasLinks ? (
+                  <Fragment>
+                      <Dropdown.DropdownLink
+                          variant='plain'
+                          href={href}
+                          text={text}
+                          icon={icon}
+                          target={target}
+                          id={id}
+                          chevron={chevron}
+                          className={sparkComponentClassName('Masthead', 'link')}
+                      />
+                      <Dropdown.DropdownLinksContainer id={id} className={right ? sparkClassName('utility', 'Right', null, 'zero') : ''}>
+                          <Dropdown.DropdownLinks links={links} />
+                    </Dropdown.DropdownLinksContainer>
+                </Fragment>
+              ) : (
+                  <Fragment>
+                      {buttonVariant ? (
+                          <Button
+                              href={href}
+                              variant={buttonVariant}
+                              onClick={onClick}
+                              className={sparkComponentClassName('Button', null, 'compact')}
+                          >
+                              {typeof text === 'function' ? text() : text}
+                        </Button>
+                      ) : (
+                          <Link
+                              href={href}
+                              variant={variant}
+                              className={this.linkClassName}
+                              target={target}
+                              style={{ textDecoration: 'none' }}
+                              onClick={onClick}
+                          >
+                              {text || <Icon name={icon} size={Icon.size.L} color='base' />}
+                        </Link>
+                      )}
+                </Fragment>
+              )}
+        </li>
+      )
   };
 }
 
